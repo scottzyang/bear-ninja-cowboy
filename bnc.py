366c0e5ed03da6b11856bc6413c1973f447b32c8
scottzyang <scottzyang@gmail.com>
2022-09-02 14:52:01 -0500

    Create function for game logic

--- bnc.py
+++ bnc.py
@@ -1,26 +1,8 @@
 # Import the random method from the randint module
 from random import randint
 
-# Define roles
-roles = ["Bear", "Ninja", "Cowboy"]
-
-# declare a variable called play and assigned to boolean False
-play = False
-
-# initiates game logic
-while play == False:
-    # verifies if user input exist as valid character
-    while True:
-        # define roles of player and computer
-        player = input("Bear, Ninja, or Cowboy? > " )
-        computer = roles[randint(0,2)]
-
-        if player not in roles:
-            print("Please input a valid character")
-        else:
-            break
-    
-    # if character exists, compare the computer role with user role
+# game logic to determine winner 
+def win_or_lose(player, computer):
     if player == computer:
         print("Draw!")
     elif computer == "Cowboy":
@@ -39,10 +21,37 @@ while play == False:
         else: # computer is ninja, player is bear
             print("You win!", player, "eats", computer)
 
+# Intro
+print("Get ready to play Bear, Ninja Cowboy!")
+
+# Instructions to play
+instructions = input("Would you like instructions on how to play? (yes/no) ")
+if instructions.lower() == "yes":
+    print("Select a player you'd like, and see if you can beat the computer!")
+        
+
+# Define roles 
+roles = ["Bear", "Ninja", "Cowboy"]
+
+# declare a variable called play and assigned to boolean False
+player = False
+
+# initiates game logic
+while player == False:
+    # verifies if user input exist as valid character
+    while True:
+        player = input("Bear, Ninja, or Cowboy? > " ) # Prompts user for player selection
+        computer = roles[randint(0,2)]
+        if player not in roles:
+            print("Please input a valid character")
+        else:
+            break
+
+    win_or_lose(player, computer)
 
     # prompt user if they want to play again
     play_again = input("Would you like to play again? (yes/no) > ")
     if play_again == 'yes':
-      play = False
+      player = False
     else:
       break
