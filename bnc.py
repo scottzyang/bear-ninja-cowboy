b2ff173358d3b7eccd9ac0c232fe830e7750f6f0
scottzyang <scottzyang@gmail.com>
2022-09-02 13:08:36 -0500

    Reformat order of program

--- bnc.py
+++ bnc.py
@@ -1,13 +1,13 @@
-player = input("Bear, Ninja, or Cowboy? > " )
-print(player)
-
 # Import the random method from the randint module
 from random import randint
 
 # Define roles
 roles = ["Bear", "Ninja", "Cowboy"]
 
+# Define player role
+player = input("Bear, Ninja, or Cowboy? > " )
+
 # Generate a random role using an array
 computer = roles[randint(0,2)]
 
-print(computer)
+print(computer, player)
