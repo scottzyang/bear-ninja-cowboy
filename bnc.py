90c6eadd21fec6aae583124e41e02e8931d85e29
scottzyang <scottzyang@gmail.com>
2022-09-02 13:31:45 -0500

    Add if conditions for game logic

--- bnc.py
+++ bnc.py
@@ -4,10 +4,31 @@ from random import randint
 # Define roles
 roles = ["Bear", "Ninja", "Cowboy"]
 
-# Define player role
-player = input("Bear, Ninja, or Cowboy? > " )
-
 # Generate a random role using an array
 computer = roles[randint(0,2)]
 
-print(computer, player)
+player = False
+
+while player == False:
+    player = input("Bear, Ninja, or Cowboy? > " )
+    
+    if player == computer:
+        print("Draw!")
+    elif computer == "Cowboy":
+        if player == "Bear":
+            print(f"You lose! {computer} shoots {player}")
+        else: # if player is ninja
+            print(f"You win! {player} beats {computer}")
+    elif computer == "Bear":
+        if player == "Cowboy":
+            print(f"You win! {player} shoots {computer}")
+        else: # if player is ninja
+            print(f"You lose! {computer} eats {player}")
+    elif computer == "Ninja":
+        if player == "Cowboy":
+            print(f"You lose! {player} is defeated by {computer}")
+        else: # computer is ninja, player is bear
+            print("You win!", player, "eats", computer)
+
+    player = False
+    computer = roles[randint(0,2)]
