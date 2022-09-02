16ddb88a9303219bb04d9f170b98cd19b49fdbc2
scottzyang <scottzyang@gmail.com>
2022-09-02 13:02:30 -0500

    Add computer role

--- bnc.py
+++ bnc.py
@@ -1,2 +1,13 @@
 player = input("Bear, Ninja, or Cowboy? > " )
 print(player)
+
+# Import the random method from the randint module
+from random import randint
+
+# Define roles
+roles = ["Bear", "Ninja", "Cowboy"]
+
+# Generate a random role using an array
+computer = roles[randint(0,2)]
+
+print(computer)
